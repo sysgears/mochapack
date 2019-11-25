00e435dc00bc985fd0c2877fcc768bb47f8cf423
Xiao Han <zjxiaohan@gmail.com>
2019-11-25 23:14:04 +0800

    recover accidentally deleted code

--- test/integration/cli/config.test.js
+++ test/integration/cli/config.test.js
@@ -10,6 +10,31 @@ const binPath = path.relative(process.cwd(), path.join('bin', '_mocha'));
 const testSimple = path.join(fixtureDir, 'simple/simple.js');
 
 describe('cli --webpack-config', function () {
+  it('does not throw for missing default config', function (done) {	
+    exec(`node ${binPath} "${testSimple}"`, (err) => {	
+      assert.isNull(err);	
+      done();	
+    });	
+  });	
+
+  it('throws when not found', function (done) {	
+    const configNotFound = 'xxxxxxx.js';	
+    exec(`node ${binPath} --webpack-config ${configNotFound} "${testSimple}"`, (err) => {	
+      assert.include(err.message, `Webpack config could not be found: ${configNotFound}`);	
+      done();	
+    });	
+  });	
+
+  it('passes --webpack-env random to config', function (done) {	
+    const config = path.join(fixtureDir, 'config/config.env.js');	
+    const env = Math.random();	
+    exec(`node ${binPath} --webpack-config ${config} --webpack-env ${env} "${testSimple}"`, (err, output) => {	
+      assert.isNull(err);	
+      assert.include(output, env);	
+      done();	
+    });	
+  });
+
   it('passes --webpack-env object to config', function (done) {
     const config = path.join(fixtureDir, 'config/config.env.js');
     const env = Math.random();
