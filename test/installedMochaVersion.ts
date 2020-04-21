5a10721ffec996eb9e46d6af9d8305162eca2357
Jack Barry <jackaldenbarry@gmail.com>
2020-04-21 12:27:00 -0500

    Fix major version checking

--- test/installedMochaVersion.ts
+++ test/installedMochaVersion.ts
@@ -3,4 +3,7 @@ const installedMochaVersion = mochaPkgJson.then(pkg => pkg.version)
 const installedMochaVersionArray = installedMochaVersion.then(v =>
   v.split('.').map(n => parseInt(n, 10))
 )
-export const installedMochaMajor = installedMochaVersionArray[0]
+
+export const installedMochaMajor = installedMochaVersionArray.then(
+  arr => arr[0]
+)
